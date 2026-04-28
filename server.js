0a9756abb4ba6e656f48af41a041d7ee580dce05
Fatma Omar <fatmaomat631@gmail.com>
2026-04-28 11:33:54 +0300

    update server & vercel

--- server.js
+++ server.js
@@ -1,3 +1,33 @@
+// import express from "express";
+// import dotenv from "dotenv";
+// import { dbConnection } from "./config/database.js";
+// import accountRoutes from "./routes/accountRoutes.js";
+// import categoryRouter from "./routes/categoryRoutes.js";
+// import serviceRouter from "./routes/serviceRoutes.js";
+// import weekDayRouter from "./routes/weekDayRoutes.js";
+// import { globalError } from "./middelware/errorMiddelware.js";
+
+// dotenv.config({ path: "./config.env" });
+// // Database Connection
+// dbConnection();
+// // Express App
+// const app = express();
+// // Middleware to parse JSON bodies
+// app.use(express.json());
+
+// app.use("/api/v1/accounts", accountRoutes);
+// app.use("/api/v1/categories", categoryRouter);
+// app.use("/api/v1/services", serviceRouter);
+// app.use("/api/v1/weekDays", weekDayRouter);
+
+// // Global Error Handling Middleware for express
+// app.use(globalError);
+// // Port
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
 import express from "express";
 import dotenv from "dotenv";
 import { dbConnection } from "./config/database.js";
@@ -8,22 +38,26 @@ import weekDayRouter from "./routes/weekDayRoutes.js";
 import { globalError } from "./middelware/errorMiddelware.js";
 
 dotenv.config({ path: "./config.env" });
-// Database Connection
+
+// DB
 dbConnection();
-// Express App
+
 const app = express();
-// Middleware to parse JSON bodies
+
 app.use(express.json());
 
+// test route
+app.get("/", (req, res) => {
+  res.send("Dawark API is running 🚀");
+});
+
+// routes
 app.use("/api/v1/accounts", accountRoutes);
 app.use("/api/v1/categories", categoryRouter);
 app.use("/api/v1/services", serviceRouter);
 app.use("/api/v1/weekDays", weekDayRouter);
 
-// Global Error Handling Middleware for express
+// error handling
 app.use(globalError);
-// Port
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
+export default app;
